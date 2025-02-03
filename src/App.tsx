486a7df8319a51d3fc0b2d0a5aee9409b993f62a
roma1524 <kulikr.js@mail.ru>
2025-02-03 15:53:29 +0300

    practice with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
+// @ts-ignore
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Routes} from "react-router-dom";
+import { Route } from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
@@ -8,10 +15,23 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <ul className={styles.navBar}>
+                        <li><NavLink to="/page1" className={({isActive}) => (isActive ? styles.activeNavLink : styles.navLink)}>Page 1</NavLink></li>
+                        <li><NavLink to="/page2" className={({isActive}) => (isActive ? styles.activeNavLink : styles.navLink)}>Page 2</NavLink></li>
+                        <li><NavLink to="/page3" className={({isActive}) => (isActive ? styles.activeNavLink : styles.navLink)}>Page 3</NavLink></li>
+                    </ul>
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+                    <Routes>
+                        <Route path="/" element={<Navigate to={'/page1'}/>}/>
+                        <Route path="/page1" element={<PageOne/>}/>
+
+                        <Route path="/page2" element={<PageTwo/>}/>
+                        <Route path="/page3" element={<PageThree/>}/>
+
+                        <Route path="/error404" element={<Error404/>}/>
+                        <Route path="/*" element={<Navigate to={'/error404'}/>}/>
+                    </Routes>
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
-  );
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
